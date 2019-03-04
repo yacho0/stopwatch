62bffdb8af337ed0e1abf2df21eab4e1d6715696
unknown <janspawel@gmail.com>
2019-03-04 23:06:54 +0100

    Solution warnings from console.

--- src/Interface.js
+++ src/Interface.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Interface.css'
 import play from './play.png';
 import pause from './pause.png';
 import stop from './stop.png';
 
-
 const Interface = (props) => {
     return (
         <div>
-            <img style={props.stateRun ? {display : 'inline'} : {display : 'none'}} className="Play-image" src={play} alt="my image" onClick={props.constrolWatch} />
-            <img style={props.stateRun ? {display : 'none'} : {display : 'inline'}} className="Pause-image" src={pause} alt="my image" onClick={props.constrolWatch} />
-            <img style={props.stateStop ? {display : 'inline'} : {display : 'none'}} className="Stop-image" src={stop} alt="my image" onClick={props.stopWatch} />
+            <img style={props.stateRun ? {display : 'inline'} : {display : 'none'}} className="Play-image" src={play} alt="my1" onClick={props.constrolWatch} />
+            <img style={props.stateRun ? {display : 'none'} : {display : 'inline'}} className="Pause-image" src={pause} alt="my2" onClick={props.constrolWatch} />
+            <img style={props.stateStop ? {display : 'inline'} : {display : 'none'}} className="Stop-image" src={stop} alt="my3" onClick={props.stopWatch} />
         </div>
     );
 };
