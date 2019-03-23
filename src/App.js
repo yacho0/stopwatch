03b065804cb51f59ebaa50e1f56d81f4d85d610e
unknown <janspawel@gmail.com>
2019-03-23 21:38:21 +0100

    you can append new stopwatch

--- src/App.js
+++ src/App.js
@@ -5,13 +5,30 @@ import StopWatch from './StopWatch';
 
 class App extends Component {
 
+  state = {
+    numChildren : 1
+  }
+
+  onAddChild = () => {
+    this.setState({
+      numChildren: this.state.numChildren + 1
+    });
+  }
+
   render() {
+
+    const children = [];
+
+    for (var i = 0; i < this.state.numChildren; i += 1) {
+      children.push(<StopWatch />);
+    };
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo"alt="logo"  />
-          <StopWatch />
-          <StopWatch />
+          {children}
+          <button onClick={this.onAddChild}>Add</button>
         </header>
       </div>
     );
