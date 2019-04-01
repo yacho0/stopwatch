b4c182ba0ad071ebb3fd248303c71bf9f7785307
unknown <janspawel@gmail.com>
2019-04-01 22:48:01 +0200

    Create controller. More tidy. better structure.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import StopWatch from './StopWatch/StopWatch';
-import plus from './plus2.png'
+import Controller from './Controller/Controller'
 
 class App extends Component {
 
-  state = {
-    numChildren : 1
-  }
-
-  onAddChild = () => {
-    this.setState({
-      numChildren: this.state.numChildren + 1
-    });
-  }
-
   render() {
-
-    const children = [];
-
-    for (var i = 0; i < this.state.numChildren; i += 1) {
-      children.push(<StopWatch key={i}/>);
-    };
 
     return (
       <div className="App">
         <header className="App-header">
           <img hidden src={logo} className="App-logo"alt="logo"  />
-          {children}
-          <img className="App-plus" alt="plus" src={plus} onClick={this.onAddChild} />
+          <Controller />
         </header>
       </div>
     );
